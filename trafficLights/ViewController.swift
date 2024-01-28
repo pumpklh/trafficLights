5708953a0ed569102210f97615c8a223a3159fef
pumpklh <127506490+pumpklh@users.noreply.github.com>
2024-01-28 21:46:44 +0600

    Initial Commit

---
+++ trafficLights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trafficLights
+//
+//  Created by Adilbek Mels on 28.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
