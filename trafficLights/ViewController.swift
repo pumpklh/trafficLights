8a7af1db97664b89ffab9c8b712a6608129051a0
pumpklh <127506490+pumpklh@users.noreply.github.com>
2024-01-29 00:16:28 +0600

    firstCommit

--- trafficLights/ViewController.swift
+++ trafficLights/ViewController.swift
@@ -9,11 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redViewLabel: UIView!
+    @IBOutlet var yellowViewLabel: UIView!
+    @IBOutlet var greenViewLabel: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    var currentLight  = 0
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        startButton.layer.cornerRadius = 10
+        redViewLabel.layer.cornerRadius = 50
+        yellowViewLabel.layer.cornerRadius = 50
+        greenViewLabel.layer.cornerRadius = 50
+        
+        startLightTraffic()
     }
+    
+    func startLightTraffic(){
+        redViewLabel.alpha = 0.3
+        yellowViewLabel.alpha = 0.3
+        greenViewLabel.alpha = 0.3
+    }
+    
+    
 
-
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        startLightTraffic()
+        
+        if currentLight == 0 {
+            redViewLabel.alpha = 1.0
+        } else if currentLight == 1 {
+            yellowViewLabel.alpha = 1.0
+        } else if currentLight == 2 {
+            greenViewLabel.alpha = 1.0
+        }
+        currentLight += 1
+        if currentLight > 2 {
+            currentLight = 0
+        }
+        if currentLight == 1 {
+            startButton.setTitle("Next", for: .normal)
+        }
+    }
+    
+    
 }
 
